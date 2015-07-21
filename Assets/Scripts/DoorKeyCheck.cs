b8a7e66e568125c2bf28ef15a67a404308bbbd21
FelixMeow <fm_matrix@hotmail.com>
2015-07-21 23:13:08 +0100

    Door functioning

---
+++ Assets/Scripts/DoorKeyCheck.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKeyCheck : MonoBehaviour
+{
+	//public AudioClip keyGrab;                       // Audioclip to play when the key is picked up.
+	
+	
+	private GameObject player;                      // Reference to the player.
+	private PlayerInventory playerInventory;        // Reference to the player's inventory.
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerInventory = player.GetComponent<PlayerInventory>();
+	}
+
+
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the colliding gameobject is the player...
+		if (other.gameObject == player && playerInventory.hasKey == true)
+		{
+			playerInventory.hasKey = false;
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour
+{
+	public bool hasKey;         // Whether or not the player has the key.
+
+
+}
