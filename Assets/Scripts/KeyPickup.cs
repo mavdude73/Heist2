8c93ead00cb61dae4adc16156d65d1be0f2a56a0
FelixMeow <fm_matrix@hotmail.com>
2015-07-22 23:44:33 +0100

    it fucking worked

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -6,22 +6,23 @@ public class KeyPickup : MonoBehaviour
 	//public AudioClip keyGrab;                       // Audioclip to play when the key is picked up.
 	
 	
-	private GameObject player;                      // Reference to the player.
+	private GameObject player1;
 	private PlayerInventory playerInventory;        // Reference to the player's inventory.
 //	private GameObject playerChild;
 	
 	void Awake ()
 	{
 		// Setting up the references.
-		player = GameObject.FindGameObjectWithTag("Player1");
-		playerInventory = player.GetComponent<PlayerInventory>();
+		player1 = GameObject.Find ("FPSController1");
+
+		playerInventory = player1.GetComponent<PlayerInventory>();
 	}
 	
 	
 	void OnTriggerEnter (Collider other)
 	{
 
-		if(other.gameObject == player)
+		if(other.gameObject == player1)
 		{
 			playerInventory.hasKey = true;
 			Destroy(gameObject);
